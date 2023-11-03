2857f84f358bc3d9848c5cba176901e3f26824f4
drago1520 <dragomir1520@gmail.com>
2023-11-03 09:28:14 +0200

    Did NOT add node.js clusters to scale horizontally. No idea how it works.

--- back-end/index.js
+++ back-end/index.js
@@ -7,6 +7,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(path.dirname(__filename));
 const frontEnd = path.join(__dirname, 'front-end');
